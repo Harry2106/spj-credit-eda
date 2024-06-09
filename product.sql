72a7dfdf4f8ff64c492cf289a264b69e68a5058c
Hoa Truong <harry.truong2106@gmail.com>
2024-06-09 18:13:23 +0700

    Delete All

--- product.sql
+++
@@ -1,8 +0,0 @@
-SELECT *
-FROM products
-WHERE sale > 1000
-      AND id < 200
-      AND date_start between '2023-01-01' and '2023-12-31'
-      AND product = 'MEOW'
-      and cust_name = 'HOA'
-      and city = 'SGN'
