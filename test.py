a1e5634d6b6489e7a74135d3dea587519649ee72
Hoa Truong <harry.truong2106@gmail.com>
2024-06-09 17:25:33 +0700

    success

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
 # %%
 # git init
 # git remote add origin <link>
+# git add <. or file_name>
+# git commit -m "Add"
+# git push -u origin main
+# git push or pull (following)
 # %%
 print(1+1)
-# %%
-print(1+)
